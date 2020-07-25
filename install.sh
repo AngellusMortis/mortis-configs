1482d2745a3e363cd143f50b6757dc29bb9aeb58
Christopher Bailey <cbailey@mort.is>
2020-07-25 14:18:16 -0400

    UPdates install

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ rm ~cbailey/dev/system-manager/group_vars/all/user.yml -f
 rm ~cbailey/dev/system-manager/group_vars/pi -f
 
 ln -s ~cbailey/dev/system-manager/files/user/mortis-configs/hosts /etc/ansible/hosts
+mkdir -p ~cbailey/dev/system-manager/files/user/mortis-configs/vars/
 ln -s ~cbailey/dev/system-manager/files/user/mortis-configs/vars/host_vars ~cbailey/dev/system-manager/host_vars
 ln -s ~cbailey/dev/system-manager/files/user/mortis-configs/vars/all.yml ~cbailey/dev/system-manager/group_vars/all/user.yml
 ln -s ~cbailey/dev/system-manager/files/user/mortis-configs/vars/pi_vars ~cbailey/dev/system-manager/group_vars/pi
